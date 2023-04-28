75e02a3e822139a9b23188a5928c4005a75bc73b
Loki7281 <charliejade8888@gmail.com>
2023-04-28 17:56:31 +0100

    bla

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
 import React, { Component } from 'react';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from './components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent'
-import Counter from './components/counter/Counter'
 import TodoApp from './components/todo/TodoApp'
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,19 +9,6 @@ class App extends Component {
       <div className="App">
          {/* <Counter/> */}
          <TodoApp/>
-      </div>
-    );
-  }
-}
-
-class LearningComponents extends Component {
-  render() {
-    return (
-      <div className="LearningComponents">
-         My Hello World
-         <FirstComponent></FirstComponent>
-         <SecondComponent></SecondComponent>
-         <ThirdComponent></ThirdComponent>
       </div>
     );
   }
